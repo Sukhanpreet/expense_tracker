d43a02fd1378cecfa1549889d09d4a6abdf0a057
Sukhanpreet <sukhanpreet356@gmail.com>
2022-07-15 21:08:31 +0530

    Change script

--- script.js
+++ script.js
@@ -14,48 +14,46 @@ function addExpense(){
     
     if(expense.value!=='' && date.value!=='' && amount.value!==''){
 
+        let arr=[];
+
+        arr[0]=expense.value;
+        arr[1]=date.value;
+        arr[2]=amount.value
+        arr[3]="";
+
         table_body.firstElementChild.setAttribute('class','nodisplay');
 
         let tr=document.createElement('tr');
-
-        table_body.append(tr);
-
-        let td1=document.createElement('td');
-        let td2=document.createElement('td');
-        let td3=document.createElement('td');
-        let td4=document.createElement('td');
-
-        tr.append(td1);
-        tr.append(td2);
-        tr.append(td3);
-        tr.append(td4);
-    
-
-        console.log(tr.previousElementSibling);
-        console.log(tr.nextElementSibling);
-
-        td1.innerText=expense.value;
-        td2.innerText=date.value;
-        td3.innerText=amount.value;
-
         let delete_button=document.createElement('input');
 
-        td4.append(delete_button);
+        table_body.append(tr);
+       
+        for(let i=0;i<4;i++){
+            let td=document.createElement('td');
+            tr.append(td);
+            
+            td.innerText=arr[i];
+        
+        }
 
+        tr.lastChild.append(delete_button);
+        
         delete_button.setAttribute('type','button');  
         delete_button.setAttribute('value','X');  
         delete_button.setAttribute('id','delete_button');  
-
 
         delete_button.addEventListener('click', deleteExpense);
 
         expense.value='';
         date.value='';
         amount.value='';
+
     }
     else{
         alert ('please fill the data');
     }
+
+    
 }
 
 
